ff40f3173739a286b810bc2a2d73a85e992d89b7
lvlu_code <1406022840@qq.com>
2021-09-13 11:43:27 +0800

    tj

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-//入口文件
+//服务器入口文件
+//创建koa实例对象
+const Koa = require('koa')
+const app = new Koa()
+//绑定中间件
+// 第一层
+const respDurationMiddleware=require('./middleware/koa_response_durations.js')
+app.use(respDurationMiddleware)
+// 第二层
+const resHead = require('./middleware/koa_response_header')
+app.use(resHead)
+// 第三层
+//绑定端口号
+app.listen(8082)
--- middleware/koa_response_durations.js
+++ middleware/koa_response_durations.js
@@ -1,1 +1,13 @@
-//计算总耗时时长
+//计算总耗时时长中间件
+module.exports = async (cxt,next)=>{
+    //记录开始的时间
+    const start = Date.now()
+    //让内层中间件得到执行
+    await next()
+    //记录结束时间‘
+    const end = Date.now()
+    //设置响应头 X-Response-Time
+    const duration = end - start
+    //ctx.set设置响应头
+    cxt.set('X-Response-Time',duration + 'ms')
+}
